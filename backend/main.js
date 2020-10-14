3182e67dddec8260fa55adc86a2a3b8b609523b0
Tom Dwyer <tom.dwyer@extropy.io>
2020-10-14 20:27:07 +0100

    Backend talking

--- backend/main.js
+++ backend/main.js
@@ -5,14 +5,21 @@ const bodyparser = require('body-parser');
 app.use(bodyparser.json()); // for parsing application/json
 app.use(bodyparser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.post('/checkScam', function (req, res) {
-    console.log('hit');
-    console.log(req);
-    res.send('hey');
+app.post('/checkPost', function (req, res) {
+  console.log('hit');
+  // console.log(req);
+  res.send('hey');
+});
+
+app.get('/checkScam', function (req, res) {
+  console.log('hit');
+  console.log(req.query);
+  // console.log(req);
+  res.send(false);
 });
 
 var server = app.listen(8081, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log('Example app listening at http://%s:%s', host, port);
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
 });
